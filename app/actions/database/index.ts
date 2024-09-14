f17dd4f1c1cb79567c40e52a22ec34aba2b32a28
Anish Araz <anisharaz919@gmail.com>
2024-09-14 21:03:55 +0530

    FEAT: multiple wallet creation & deletion in profile

--- app/actions/database/index.ts
+++ app/actions/database/index.ts
@@ -237,3 +237,121 @@ export async function UpdateNickName({
     };
   }
 }
+
+export async function AddUserWallet({
+  pin,
+  email,
+  chain,
+  walletname,
+}: {
+  pin: string;
+  email: string;
+  chain: $Enums.Chain;
+  walletname: string;
+}) {
+  const session = await auth();
+  if (!session) {
+    return {
+      success: false,
+      message: "Unauthorized",
+    };
+  }
+  try {
+    const userDB = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+      include: {
+        UserWallet: true,
+      },
+    });
+    const keypair = Keypair.generate();
+    const getSalt = nacl.randomBytes(16);
+    const key = pbkdf2Sync(pin, getSalt, 100000, SecretBoxLength.Key, "sha512");
+    const nonce = nacl.randomBytes(SecretBoxLength.Nonce);
+    const messageUint8 = keypair.secretKey.buffer;
+    const encryptedPrivateKey = nacl.secretbox(
+      Buffer.from(messageUint8),
+      nonce,
+      key
+    );
+    await prisma.$transaction([
+      prisma.userWallet.create({
+        data: {
+          userId: userDB?.id as string,
+          chain: chain,
+          walletname: walletname,
+          publicKey: keypair.publicKey.toString(),
+          privateKey: encryptedPrivateKey.toString(),
+          salt_for_pin: getSalt.toString(),
+          nonce_for_encryption: nonce.toString(),
+        },
+      }),
+      prisma.user.update({
+        where: {
+          email: email,
+        },
+        data: {
+          initalized: true,
+        },
+      }),
+    ]);
+
+    return {
+      success: true,
+      message: "Wallet created successfully",
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      success: false,
+      message: "Error creating wallet",
+    };
+  }
+}
+
+export async function DeleteUserWallet({
+  email,
+  walletId,
+  walletname,
+}: {
+  email: string;
+  walletId: string;
+  walletname: string;
+}) {
+  const session = await auth();
+  if (!session || session.user?.email != email) {
+    return {
+      success: false,
+      message: "Unauthorized",
+    };
+  }
+  const wallet = await prisma.userWallet.findUnique({
+    where: {
+      id: walletId,
+    },
+  });
+  if (wallet?.walletname == "Main") {
+    return {
+      success: false,
+      message: "Cannot delete main wallet",
+    };
+  }
+  try {
+    await prisma.userWallet.delete({
+      where: {
+        id: walletId,
+      },
+    });
+    return {
+      success: true,
+      message: "Wallet deleted",
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      success: false,
+      message: "Error deleting wallet",
+    };
+  }
+}
