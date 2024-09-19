102438a42b9584bb56e33f8a25d3a899746e0826
PiyushXCoder <piyush.raj.kit@gmail.com>
2024-09-19 15:44:00 +0530

    UI: Enhance UI for homepage
    Also:
    * Change favicon
    * Use permanent redirect for `/user`

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,6 +1,6 @@
-import { redirect } from "next/navigation";
+import { permanentRedirect } from "next/navigation";
 function UserPage() {
-  redirect("/user/wallet/send");
+  permanentRedirect("/user/wallet/send");
 }
 
 export default UserPage;
